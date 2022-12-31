e3d4d5e5176b5b64a3cabc3c8c4874aa692f3bf1
sudip40 <karmakar.sudip40@gmail.com>
2022-12-31 21:55:56 +0530

    Enum

---
+++ Enum/src/com/enums/Main.java
@@ -0,0 +1,18 @@
+package com.enums;
+
+enum Days {
+	SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Days day1 = Days.SUNDAY;
+		Days day2 = Days.MONDAY;
+		Days day3 = Days.TUESDAY;
+		Days day4 = Days.WEDNESDAY;
+		Days day5 = Days.THURSDAY;
+		Days day6 = Days.FRIDAY;
+		Days day7 = Days.SATURDAY;
+		System.out.println(day5);
+	}
+}
